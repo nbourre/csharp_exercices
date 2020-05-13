db67e09ac2cf7a3d7dc3b010cc566ee5f39aaa35
Nicolas Bourré <nbourrereg@hotmail.com>
2020-05-13 14:18:25 -0400

    wpf_guess de base complet

--- wpf_guess/MainWindow.xaml.cs
+++ wpf_guess/MainWindow.xaml.cs
@@ -20,9 +20,41 @@ namespace wpf_guess
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        int valueToGuess;
+        readonly int minValue = 1;
+        readonly int maxValue = 1000;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            valueToGuess = getNewValue();
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            valueToGuess = getNewValue();
+        }
+
+
+
+        private int getNewValue() => rnd.Next(minValue, maxValue);
+
+        private void Validate_Click(object sender, RoutedEventArgs e)
+        {
+            int guess = Convert.ToInt32(txtNumber.Text);
+
+            if (guess == valueToGuess)
+            {
+                txtMessage.Text = "Bravo! Tu as trouvé. Appuie sur Recommencer pour refaire une partie.";
+            } else if (guess < valueToGuess)
+            {
+                txtMessage.Text = "Trop bas!";
+            } else
+            {
+                txtMessage.Text = "Trop haut!";
+            }
         }
     }
 }
