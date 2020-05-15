30e5409d349252cbd6c31cd484cebc07ebccd339
Nicolas Bourré <nbourrereg@hotmail.com>
2020-05-15 13:38:48 -0400

    Code de base pour add_numbers complété

---
+++ add_numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace add_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombreA, nombreB, resultat;
+            Console.WriteLine("Entrez le premier nombre : ");
+            nombreA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez le second nombre : ");
+            nombreB = int.Parse(Console.ReadLine());
+
+            resultat = nombreA + nombreB;
+
+            Console.WriteLine($"Le résultat de l'addition est {resultat}");
+
+        }
+    }
+}
