4eabb922c70f1d919e71ef51c0171c75a6a44081
Nicolas Bourré <nbourrereg@hotmail.com>
2020-05-15 11:32:50 -0400

    Code pour l'exercice convertir la boucle for

---
+++ convert_loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace convert_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Début de la boucle \"for\"");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine($"  i = {i}");
+            }
+
+            Console.WriteLine("Terminé!");
+        }
+    }
+}
